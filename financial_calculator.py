ea0d0dc3d9a3cd09d6c7553c464799b5ae53ec70
Lucas Bishop <57057247+Bishop-Lucas@users.noreply.github.com>
2020-07-19 14:14:07 -0500

    Finished product refactored

--- financial_calculator.py
+++ financial_calculator.py
@@ -1,7 +1,7 @@
 #import numpy
 import math
 
-active = 'y'
+
 ### Creating the functions for 
 def get_interest():
     # interest rate
@@ -61,27 +61,30 @@ def compound_sf():
     csfr = round(csf,2)
     return csfr
 
-while active == 'y':
-    print("This tool lets you calculate various values of money based upon compounding interest for various terms and conditions.")
-    print("If you would like to know the value of a future sum of money given a present sum, the interest rate anumber of terms please press f")
-    print("If you would like to know the value of a present sum of money given a future sum, the interest rate anumber of terms please press p")
-    print("If you would like to know the value of a future sum of money given a set amount added every term, the interest rate anumber of terms please press a")
-    print("If you would like to know the value needed every term for given a future sum, the interest rate anumber of terms please press r")
-    response = input()
-    if response == 'f':
-        fv = compound_f()
-        print("The future value is " + str(fv))
-    elif response == 'p':
-        pv = compound_p()
-        print("The present value is " + str(pv))
-    elif response == 'a':
-        av = compound_s()
-        print("The present value is " + str(av))
-    elif response == 'r':
-        rv = compound_sf()
-        print("The present value is " + str(rv))
-    else:
-        print('Your selection was not valid')
-        
-    print("Would you like to calculate another value?(y/n)")
-    active = input()
+def calculator():
+    active = 'y'
+    while active == 'y':
+        print("This tool lets you calculate various values of money based upon compounding interest for various terms and conditions.")
+        print("If you would like to know the value of a future sum of money given a present sum, the interest rate anumber of terms please press f")
+        print("If you would like to know the value of a present sum of money given a future sum, the interest rate anumber of terms please press p")
+        print("If you would like to know the value of a future sum of money given a set amount added every term, the interest rate anumber of terms please press a")
+        print("If you would like to know the value needed every term for given a future sum, the interest rate anumber of terms please press r")
+        response = input()
+        if response == 'f':
+            fv = compound_f()
+            print("The future value is " + str(fv))
+        elif response == 'p':
+            pv = compound_p()
+            print("The present value is " + str(pv))
+        elif response == 'a':
+            av = compound_s()
+            print("The present value is " + str(av))
+        elif response == 'r':
+            rv = compound_sf()
+            print("The present value is " + str(rv))
+        else:
+            print('Your selection was not valid')
+            
+        print("Would you like to calculate another value?(y/n)")
+        active = input()
+calculator()
